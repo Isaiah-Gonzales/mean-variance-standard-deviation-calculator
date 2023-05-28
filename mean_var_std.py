8b9d4d05cddbe49ad255685a41f075049161a0e2
theroseknowz <110946128+theroseknowz@users.noreply.github.com>
2023-05-28 19:33:50 +0000

    Code passing all tests

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 def calculate(list):
+  if len(list) != 9:
+    raise ValueError("List must contain nine numbers.") 
+  array = np.array([list[0:3],list[3:6],list[6:9]])
+  calculations = {
+    'mean':[array.mean(axis=0).tolist(),array.mean(axis=1).tolist(),array.mean().tolist()],
+    'variance':[array.var(axis=0).tolist(),array.var(axis=1).tolist(),array.var().tolist()],
+    'standard deviation':[array.std(axis=0).tolist(),array.std(axis=1).tolist(),array.std().tolist()],
+    'max':[array.max(axis=0).tolist(),array.max(axis=1).tolist(),array.max().tolist()],
+    'min':[array.min(axis=0).tolist(),array.min(axis=1).tolist(),array.min().tolist()],
+    'sum':[array.sum(axis=0).tolist(),array.sum(axis=1).tolist(),array.sum().tolist()]
+  }
 
-
-
-
-    return calculations
+  return calculations
